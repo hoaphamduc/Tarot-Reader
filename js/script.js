9a7cb4d1ef27d913d9b6cc9085bfdaf3f1d4c003
hoaphamduc <hoaphamduc2399@gmail.com>
2024-05-07 01:29:27 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+// // Chặn việc mở DevTools bằng phím tắt
+// window.addEventListener('keydown', function (event) {
+//   if (event.keyCode == 123) { // 123 là mã phím tắt cho DevTools trên nhiều trình duyệt
+//     event.preventDefault();
+//   }
+// });
+// // Chặn việc mở DevTools bằng phím tắt và tổ hợp phím
+// window.addEventListener('keydown', function (event) {
+//   // Kiểm tra xem người dùng có nhấn tổ hợp phím tắt DevTools không
+//   if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || // Ctrl+Shift+I
+//       (event.ctrlKey && event.shiftKey && event.keyCode == 74) || // Ctrl+Shift+J
+//       (event.ctrlKey && event.keyCode == 85) || // Ctrl+U
+//       (event.ctrlKey && event.shiftKey && event.keyCode == 67) || // Ctrl+Shift+C
+//       (event.ctrlKey && event.keyCode == 83)) { // Ctrl+S
+//     event.preventDefault();
+//   }
+// });
+
+// // Chặn việc mở DevTools bằng chuột phải
+// window.addEventListener('contextmenu', function (event) { 
+//   event.preventDefault();
+//   console.log('DevTools đã bị chặn.');
+// });
+
+document.addEventListener("copy", function(e) {
+    e.preventDefault();
+    alert("Bạn không được phép copy.");
+});
+
 var isMusicPlaying = false;
 var backgroundMusic = document.getElementById("backgroundMusic");
 var intervalId;
